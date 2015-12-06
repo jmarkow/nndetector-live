ba5158aa406354ea13e8195945431f6df4083458
Jeff Markowitz <markowitzmeister@gmail.com>
2015-12-06 12:59:52 -0500

    Getting closer...

--- nndetector_live_sim_network.m
+++ nndetector_live_sim_network.m
@@ -6,12 +6,12 @@ function [ACTIVATION,TRIGGER]=nndetector_live_sim_network(INPUT,NETWORK)
 
 % first layer
 
-ACTIVATION=NETWORK.transfer_function{1}(INPUT'*NETWORK.layer_weights{1}+NETWORK.layer_biases{1});
+ACTIVATION=(NETWORK.transfer_function{1}(INPUT'*NETWORK.layer_weights{1}'+NETWORK.layer_biases{1}'));
 
 % propagate through additional layers
 
-for i=2:length(NETWORK.weights)
-  ACTIVATION=NETWORK.transfer_function{i}(ACTIVATION'*NETWORK.layer_weights{i}+NETWORK.layer_biases{i});
+for i=2:length(NETWORK.layer_weights)
+  ACTIVATION=NETWORK.transfer_function{i}(ACTIVATION*NETWORK.layer_weights{i}'+NETWORK.layer_biases{i}');
 end
 
 TRIGGER=ACTIVATION>=NETWORK.threshold;
--- nndetector_live_simulate.m
+++ nndetector_live_simulate.m
@@ -3,7 +3,11 @@ function nndetector_live_simulate(INPUT_DEVICE,OUTPUT_DEVICE,TEST_FILE,FS,QUEUE_
 % put out detector and actual hits on left/right channels for line out
 
 fprintf('Loading file: %s\n',TEST_FILE)
-dsp_obj_file=dsp.AudioFileReader(TEST_FILE,'SamplesPerFrame',NETWORK.spec_params.win_size); % for now assume left channel is audio data
+
+NETWORK.spec_params.win_overlap=NETWORK.spec_params.win_size-NETWORK.spec_params.fft_time_shift;
+samples_per_frame=NETWORK.spec_params.fft_time_shift*NETWORK.spec_params.time_steps;
+
+dsp_obj_file=dsp.AudioFileReader(TEST_FILE,'SamplesPerFrame',samples_per_frame); % for now assume left channel is audio data
 
 fprintf('Setting up AudioPlayer on %s\n',OUTPUT_DEVICE);
 %dsp_obj_out=dsp.AudioPlayer('SampleRate',FS,'DeviceName',OUTPUT_DEVICE,'QueueDuration',QUEUE_SIZE_OUTPUT);
@@ -15,18 +19,24 @@ fprintf('Entering file play loop...\n');
 % validate frequency and time indices (maybe preflight function?)
 
 freq_idx=NETWORK.spec_params.freq_range_ds(1):NETWORK.spec_params.freq_range_ds(end);
-
+fig=figure(1);
+fi2=figure(2);
 while ~isDone(dsp_obj_file)
   tic;
   audio_data=step(dsp_obj_file);
 
-  size(audio_data)
-
+  if size(audio_data,1)<samples_per_frame
+    break;
+  end
   % scale, normalize, etc. etc.
 
-  s=spectrogram(audio_data,NETWORK.spec_params.win_size,NETWORK.spec_params.win_shift,NETWORK.spec_params.fft_size);
-  s=s(freq_idx,:);
+  s=spectrogram(audio_data(:,1),NETWORK.spec_params.win_size,NETWORK.spec_params.win_overlap,NETWORK.spec_params.fft_size);
+  s=abs(s(freq_idx,:));
   s=NETWORK.amp_scaling_fun(s);
+
+  figure(1);
+  imagesc(s)
+
   s=reshape(s,[],1);
   s=NETWORK.input_normalize(s);
 
@@ -36,7 +46,15 @@ while ~isDone(dsp_obj_file)
 
   activation
   trigger
+
+  if any(trigger)
+    figure(2);plot(audio_data(:,2))
+    pause();
+  end
+
   toc
+
+  pause(.1);
 
   % active or inactive?
 
